04c7ad72a0257ad9e71c9aa3d438772f16709597
ArijitR111Y <royarijit998@gmail.com>
2019-12-25 17:01:56 +0530

    Added: Login & Register Activity (Layout only)

--- app/src/main/java/com/royarijit998/vlearnit/MainActivity.java
+++ app/src/main/java/com/royarijit998/vlearnit/MainActivity.java
@@ -1,6 +1,7 @@
 package com.royarijit998.vlearnit;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.RecyclerView;
@@ -8,22 +9,36 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toast;
+import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     private NavigationView mainNavigationView;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
     private DrawerLayout mainDrawerLayout;
     private RecyclerView userPostRecyclerView;
+    private Toolbar mainToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mainToolbar = findViewById(R.id.mainToolbar);
+        setSupportActionBar(mainToolbar);
+        getSupportActionBar().setTitle("Home");
+
         mainNavigationView = findViewById(R.id.mainNavigationView);
         mainDrawerLayout = findViewById(R.id.mainDrawerLayout);
+        //Step-1 : Create an actionBarDrawerToggle object
+        actionBarDrawerToggle  = new ActionBarDrawerToggle(MainActivity.this, mainDrawerLayout, R.string.drawer_open, R.string.drawer_close);
+        //Step-2 : Add drawer listener to the drawerLayout
+        mainDrawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        //Step -3 : Set corresponding parameter to the supportActionBar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         userPostRecyclerView = findViewById(R.id.userPostRecyclerView);
         mainNavigationView.inflateHeaderView(R.layout.navigation_header);
 
@@ -35,6 +50,14 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(actionBarDrawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void raiseToastForSelection(MenuItem menuItem) {
