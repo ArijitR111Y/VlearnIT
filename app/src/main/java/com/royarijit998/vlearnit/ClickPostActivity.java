2f4df87e4bd10b63ed41dab62fab473f8b5f0bfb
ArijitR111Y <royarijit998@gmail.com>
2020-01-03 02:58:43 +0530

    Added: Edit and update post in ClickPostActivity

--- app/src/main/java/com/royarijit998/vlearnit/ClickPostActivity.java
+++ app/src/main/java/com/royarijit998/vlearnit/ClickPostActivity.java
@@ -1,14 +1,19 @@
 package com.royarijit998.vlearnit;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -24,7 +29,7 @@ public class ClickPostActivity extends AppCompatActivity {
     private TextView postDescTextView;
     private Button deletePostBtn, editPostBtn;
 
-    private String postKey, currUserID, databaseUserID;
+    private String postKey, currUserID, databaseUserID, postDescription;
 
     private DatabaseReference postRef;
     private FirebaseAuth currUser;
@@ -56,7 +61,7 @@ public class ClickPostActivity extends AppCompatActivity {
                 if(dataSnapshot.exists()){
                     databaseUserID = dataSnapshot.child("uid").getValue().toString();
 
-                    String postDescription = dataSnapshot.child("description").getValue().toString();
+                    postDescription = dataSnapshot.child("description").getValue().toString();
                     postDescTextView.setText(postDescription);
 
                     String imagePath = dataSnapshot.child("postImg").getValue().toString();
@@ -82,11 +87,47 @@ public class ClickPostActivity extends AppCompatActivity {
             }
         });
 
+        editPostBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateCurrentPost();
+            }
+        });
+
+    }
+
+    private void updateCurrentPost() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(ClickPostActivity.this);
+        builder.setTitle("Edit Post Description");
+
+        final EditText inputField = new EditText(ClickPostActivity.this);
+        inputField.setText(postDescription);
+        builder.setView(inputField);
+
+        builder.setPositiveButton("Update", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                postRef.child("description").setValue(inputField.getText().toString());
+                Toast.makeText(ClickPostActivity.this, "Post Description Updated", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        Dialog dialog = builder.create();
+        dialog.show();
+
     }
 
     private void deleteCurrentPost() {
         postRef.removeValue();
         sendUserToHomeActivity();
+        Toast.makeText(this, "Post deleted successfully", Toast.LENGTH_SHORT).show();
     }
 
     private void sendUserToHomeActivity() {
